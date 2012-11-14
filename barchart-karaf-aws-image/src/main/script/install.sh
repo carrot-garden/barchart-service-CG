3aeb4bfe19826c0e71b4fd361a136945c12a3db9
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2012-11-14 15:07:45 -0600

    cleanup install

--- barchart-karaf-aws-image/src/main/script/install.sh
+++ barchart-karaf-aws-image/src/main/script/install.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
 
+ADMIN_GROUP="root"
+ADMIN_USER="ubuntu"
+
+KARAF_HOME="/var/karaf"
+KARAF_GROUP="karaf"
+KARAF_USER="karaf"
+
+KARAF_ARCHIVE="${karafFinalName}"
+KARAF_APP_DIR="app"
+
+# identity during setup
+export BARCHART_CONFIG_IDENTITY="default.aws.barchart.com"
+
 # script location
 WORK=$(dirname $0 )
 
 echo "##################################################"
-
-echo "project : ${project.artifactId}"
+echo "### project : ${project.artifactId}"
+echo "###"
 
 echo "##################################################"
+echo "### verify work location"
+echo "###"
 
-# verify location
 pwd
 
 echo "##################################################"
+echo "### verify upload contents"
+echo "###"
 
-# verify upload
 ls -las $WORK
 
 echo "##################################################"
+echo "### configure kernel"
+echo "###"
 
-# disable apt-get and oracle dialogs
+cp --verbose --force $WORK/sysctl-karaf.conf /etc/sysctl.d/
+sysctl -p
+
+echo "##################################################"
+echo "### disable installer dialogs"
+echo "###"
+
 export DEBIAN_FRONTEND=noninteractive
 echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
 	
-# install applications
+echo "##################################################"
+echo "### install utilities"
+echo "###"
 add-apt-repository --yes ppa:webupd8team/java
 apt-get --yes update
 apt-get --yes install oracle-jdk7-installer
@@ -33,43 +58,77 @@ apt-get --yes upgrade
 apt-get --yes dist-upgrade
 
 echo "##################################################"
-						
-# verify java
+echo "### verify java"
+echo "###"
+																																																
 java -version 2>&1
 
 echo "##################################################"
+echo "### setup karaf user home"
+echo "###"
 
-ADMIN_GROUP="ubuntu"
-ADMIN_USER="ubuntu"
-
-KARAF_HOME="/var/karaf"
-KARAF_GROUP="karaf"
-KARAF_USER="karaf"
-
-KARAF_ARCHIVE="${karafFinalName}"
-
-# setup karaf user
 addgroup --system $KARAF_GROUP
-adduser --system --ingroup $KARAF_GROUP --home $KARAF_HOME $KARAF_USER
-adduser $ADMIN_USER $KARAF_GROUP
+adduser  --system --shell=/bin/bash --ingroup $KARAF_GROUP --home $KARAF_HOME $KARAF_USER
 
-# restrict karaf home access
-chown --changes --recursive $ADMIN_GROUP:$KARAF_USER $KARAF_HOME
+echo "##################################################"
+echo "### copy karaf home resources"
+echo "###"
+
+cp --verbose --force --recursive $WORK/. $KARAF_HOME
+
+echo "##################################################"
+echo "### unpack karaf application"
+echo "###"
+
+unzip -o $KARAF_HOME/$KARAF_ARCHIVE -d $KARAF_HOME
+
+echo "##################################################"
+echo "### remove application folder time stamp suffix"
+echo "###"
+
+mv --verbose "$KARAF_HOME/$KARAF_APP_DIR"* "$KARAF_HOME/$KARAF_APP_DIR" 
+
+echo "##################################################"
+echo "### setup karaf home access permissions"
+echo "###"
+
+chown --changes --recursive $KARAF_USER:$KARAF_GROUP $KARAF_HOME
 chmod --changes --recursive o-rwx,g+rw,ugo-s $KARAF_HOME
 find $KARAF_HOME -type d -exec chmod --changes g+s {} \;
 
 echo "##################################################"
-
-# copy karaf home resources
-cp --recursive $WORK $KARAF_HOME
-
-# unpack karaf application
-unzip $KARAF_ARCHIVE -d $KARAF_HOME
-
-# cleanup work folder
-srm -r -l -v $WORK
+echo "### identity during setup : $BARCHART_CONFIG_IDENTITY"
+echo "###"
 
 echo "##################################################"
+echo "### install tanuki service"
+echo "###"
+
+$KARAF_HOME/$KARAF_APP_DIR/nix-install.sh
+
+echo "##################################################"
+echo "### tanuki start"
+echo "###"
+
+$KARAF_HOME/$KARAF_APP_DIR/nix-start.sh
+
+echo "##################################################"
+echo "### await fetch config repo and maven artifacts"
+echo "###"
+
+sleep 30s
+
+echo "##################################################"
+echo "### tanuki stop"
+echo "###"
+
+$KARAF_HOME/$KARAF_APP_DIR/nix-stop.sh
+
+echo "##################################################"
+echo "### cleanup work folder"
+echo "###"
+
+srm -r -l -v $WORK
 
 sleep 3s
 
