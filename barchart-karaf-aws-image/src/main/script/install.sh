6fc90086c68c78d78259c0918c2fc644166ccde3
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-11-14 09:55:29 -0600

    deploy karaf archive

--- barchart-karaf-aws-image/src/main/script/install.sh
+++ barchart-karaf-aws-image/src/main/script/install.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
+# script location
+WORK=$(dirname $0 )
+
 echo "##################################################"
 
 echo "project : ${project.artifactId}"
 
 echo "##################################################"
 
+# verify location
 pwd
 
 echo "##################################################"
 
-ls -las
+# verify upload
+ls -las $WORK
 
 echo "##################################################"
 
-#
 # disable apt-get and oracle dialogs
-#
 export DEBIAN_FRONTEND=noninteractive
 echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
 	
+# install applications
 add-apt-repository --yes ppa:webupd8team/java
 apt-get --yes update
 apt-get --yes install oracle-jdk7-installer
@@ -29,7 +33,8 @@ apt-get --yes upgrade
 apt-get --yes dist-upgrade
 
 echo "##################################################"
-												
+						
+# verify java
 java -version 2>&1
 
 echo "##################################################"
@@ -41,18 +46,27 @@ KARAF_HOME="/var/karaf"
 KARAF_GROUP="karaf"
 KARAF_USER="karaf"
 
+KARAF_ARCHIVE="${karafFinalName}"
+
+# setup karaf user
 addgroup --system $KARAF_GROUP
 adduser --system --ingroup $KARAF_GROUP --home $KARAF_HOME $KARAF_USER
 adduser $ADMIN_USER $KARAF_GROUP
 
+# restrict karaf home access
 chown --changes --recursive $ADMIN_GROUP:$KARAF_USER $KARAF_HOME
 chmod --changes --recursive o-rwx,g+rw,ugo-s $KARAF_HOME
 find $KARAF_HOME -type d -exec chmod --changes g+s {} \;
 
 echo "##################################################"
 
-WORK=$(dirname $0 )
+# copy karaf home resources
+cp --recursive $WORK $KARAF_HOME
 
+# unpack karaf application
+unzip $KARAF_ARCHIVE -d $KARAF_HOME
+
+# cleanup work folder
 srm -r -l -v $WORK
 
 echo "##################################################"
