9d00d5ae7ab0aa2015db778e514b8e5772bfe322
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2012-11-15 16:19:04 -0600

    health report

---
+++ barchart-karaf-http-health-report/src/main/java/com/barchart/karaf/test/api/HealthReport.java
@@ -0,0 +1,7 @@
+package com.barchart.karaf.test.api;
+
+public interface HealthReport {
+
+	String PATH = "/health-report";
+
+}
---
+++ barchart-karaf-http-health-report/src/main/java/com/barchart/karaf/test/impl/HealthReportComponent.java
@@ -0,0 +1,74 @@
+package com.barchart.karaf.test.impl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.joda.time.DateTime;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.HttpService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.barchart.karaf.test.api.HealthReport;
+
+@SuppressWarnings("serial")
+@Component(immediate = true, service = HealthReport.class)
+public class HealthReportComponent extends HttpServlet implements HealthReport {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	static String entry(final String key, final String value) {
+		return "\"" + key + "\"" + "=" + "\"" + value + "\"";
+	}
+
+	@Override
+	protected void doGet(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException,
+			IOException {
+
+		final String report = //
+		"{ " + entry("timestamp", new DateTime().toString()) + " }";
+
+		response.setContentType("application/json");
+
+		response.getWriter().write(report);
+
+	}
+
+	@Activate
+	protected void activate() throws Exception {
+
+		log.info("### activate : {}" + PATH);
+
+		httpService.registerServlet(PATH, this, null, null);
+
+	}
+
+	@Deactivate
+	protected void deactivate() {
+
+		httpService.unregister(PATH);
+
+		log.info("### deactivate : {}" + PATH);
+
+	}
+
+	private HttpService httpService;
+
+	@Reference
+	protected void bind(final HttpService s) {
+		httpService = s;
+	}
+
+	protected void unbind(final HttpService s) {
+		httpService = null;
+	}
+
+}
